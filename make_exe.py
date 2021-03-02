256c4a21734ace7d8c07dd5ebf8de3d9cdabb460
BWCommand286 <73004373+BWCommand286@users.noreply.github.com>
2021-03-02 19:47:30 +0100

    Create make_exe.py

---
+++ make_exe.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 20 23:34:20 2021
+
+@author: Roman
+   
+    - PyInstaller version 16.8 is required
+    
+"""
+
+import PyInstaller.__main__
+
+PyInstaller.__main__.run(['gui.py','--onefile'])
+
+
